a77c3d8febf6c4a1850d809190bbbe20f6a86034
sanderdemeyer <sander.demeyer@ugent.be>
2025-01-30 14:43:21 +0100

    update docstrings and small bugfix

--- src/hubbardoperators.jl
+++ src/hubbardoperators.jl
@@ -60,11 +60,12 @@ function two_site_operator(T, particle_symmetry::Type{<:Sector},
     return zeros(T, V ⊗ V ← V ⊗ V)
 end
 
-"""
-    u_plus_u_min(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    u_plus_u_min([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    u⁺d⁻([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the two-body operator ``c†_{1,↑}, c_{2,↑}`` that creates a spin-up particle at the first site and annihilates a spin-up particle at the second.
-"""
+""" u_plus_u_min
 u_plus_u_min(P::Type{<:Sector}, S::Type{<:Sector}) = u_plus_u_min(ComplexF64, P, S)
 function u_plus_u_min(T, ::Type{Trivial}, ::Type{Trivial})
     t = two_site_operator(T, Trivial, Trivial)
@@ -117,13 +118,14 @@ end
 function u_plus_u_min(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
     return error("Not implemented")
 end
-const u⁺d⁻ = u_plus_u_min
+const u⁺u⁻ = u_plus_u_min
 
-"""
-    d_plus_d_min(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    d_plus_d_min([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    d⁺d⁻([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the two-body operator ``c†_{1,↓}, c_{2,↓}`` that creates a spin-down particle at the first site and annihilates a spin-down particle at the second.
-"""
+""" d_plus_d_min
 d_plus_d_min(P::Type{<:Sector}, S::Type{<:Sector}) = d_plus_d_min(ComplexF64, P, S)
 function d_plus_d_min(T, ::Type{Trivial}, ::Type{Trivial})
     t = two_site_operator(T, Trivial, Trivial)
@@ -178,38 +180,41 @@ function d_plus_d_min(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
 end
 const d⁺d⁻ = d_plus_d_min
 
-"""
-    u_min_u_plus(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    u_min_u_plus([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    u⁻u⁺([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the Hermitian conjugate of `u_plus_u_min`, i.e.
 ``(c†_{1,↑}, c_{2,↑})† = -c_{1,↑}, c†_{2,↑}`` (note the extra minus sign). 
 It annihilates a spin-up particle at the first site and creates a spin-up particle at the second.
-"""
+""" u_min_u_plus
 u_min_u_plus(P::Type{<:Sector}, S::Type{<:Sector}) = u_min_u_plus(ComplexF64, P, S)
 function u_min_u_plus(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
     return copy(adjoint(u_plus_u_min(T, particle_symmetry, spin_symmetry)))
 end
 const u⁻u⁺ = u_min_u_plus
 
-"""
-    d_min_d_plus(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    d_min_d_plus([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    d⁻d⁺([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the Hermitian conjugate of `d_plus_d_min`, i.e.
 ``(c†_{1,↓}, c_{2,↓})† = -c_{1,↓}, c†_{2,↓}`` (note the extra minus sign). 
 It annihilates a spin-down particle at the first site and creates a spin-down particle at the second.
-"""
+""" d_min_d_plus
 d_min_d_plus(P::Type{<:Sector}, S::Type{<:Sector}) = d_min_d_plus(ComplexF64, P, S)
 function d_min_d_plus(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
     return copy(adjoint(d_plus_d_min(T, particle_symmetry, spin_symmetry)))
 end
 const d⁻d⁺ = d_min_d_plus
 
-"""
-    c_plus_c_min(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    c_plus_c_min([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    c⁺c⁻([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the two-body operator that creates a particle at the first site and annihilates a particle at the second.
 This is the sum of `u_plus_u_min` and `d_plus_d_min`.
-"""
+""" c_plus_c_min
 c_plus_c_min(P::Type{<:Sector}, S::Type{<:Sector}) = c_plus_c_min(ComplexF64, P, S)
 function c_plus_c_min(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
     return u_plus_u_min(T, particle_symmetry, spin_symmetry) +
@@ -251,23 +256,25 @@ function c_plus_c_min(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
 end
 const c⁺c⁻ = c_plus_c_min
 
-"""
-    c_min_c_plus(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    c_min_c_plus([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    c⁻c⁺([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the two-body operator that annihilates a particle at the first site and creates a particle at the second.
 This is the sum of `u_min_u_plus` and `d_min_d_plus`.
-"""
+""" c_min_c_plus
 c_min_c_plus(P::Type{<:Sector}, S::Type{<:Sector}) = c_min_c_plus(ComplexF64, P, S)
 function c_min_c_plus(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
     return copy(adjoint(c_plus_c_min(T, particle_symmetry, spin_symmetry)))
 end
 const c⁻c⁺ = c_min_c_plus
 
-"""
-    u_num(particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    u_num([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    nꜛ([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the one-body operator that counts the number of spin-up particles.
-"""
+""" u_num
 u_num(P::Type{<:Sector}, S::Type{<:Sector}) = u_num(ComplexF64, P, S)
 function u_num(T::Type{<:Number}, ::Type{Trivial}=Trivial, ::Type{Trivial}=Trivial)
     t = single_site_operator(T, Trivial, Trivial)
@@ -314,11 +321,12 @@ function u_num(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
 end
 const nꜛ = u_num
 
-"""
-    d_num(particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    d_num([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    nꜜ([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the one-body operator that counts the number of spin-down particles.
-"""
+""" d_num
 d_num(P::Type{<:Sector}, S::Type{<:Sector}) = d_num(ComplexF64, P, S)
 function d_num(T::Type{<:Number}, ::Type{Trivial}=Trivial, ::Type{Trivial}=Trivial)
     t = single_site_operator(T, Trivial, Trivial)
@@ -365,11 +373,12 @@ function d_num(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
 end
 const nꜜ = d_num
 
-"""
-    c_num(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    c_num([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    n([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the one-body operator that counts the number of particles.
-"""
+""" c_num
 c_num(P::Type{<:Sector}, S::Type{<:Sector}) = c_num(ComplexF64, P, S)
 function c_num(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
     return u_num(T, particle_symmetry, spin_symmetry) +
@@ -391,11 +400,12 @@ function c_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
 end
 const n = c_num
 
-"""
-    ud_num(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+@doc """
+    ud_num([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    nꜛnꜜ([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
 
 Return the one-body operator that counts the number of doubly occupied sites.
-"""
+""" ud_num
 ud_num(P::Type{<:Sector}, S::Type{<:Sector}) = ud_num(ComplexF64, P, S)
 function ud_num(T, particle_symmetry::Type{<:Sector},
                          spin_symmetry::Type{<:Sector})
