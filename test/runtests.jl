31fc6f760fda5683e3fffadb094ca42c69931bbe
sanderdemeyer <sander.demeyer@ugent.be>
2025-01-30 14:40:46 +0100

    include other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,14 @@ using SafeTestsets
     include("hubbardoperators.jl")
 end
 
-# @time @safetestset "spin operators" begin
-#     include("spinoperators.jl")
-# end
+@time @safetestset "spin operators" begin
+    include("spinoperators.jl")
+end
 
-# @time @safetestset "boson operators" begin
-#     include("bosonoperators.jl")
-# end
+@time @safetestset "boson operators" begin
+    include("bosonoperators.jl")
+end
 
-# @time @safetestset "Aqua" begin
-#     include("aqua.jl")
-# end
+@time @safetestset "Aqua" begin
+    include("aqua.jl")
+end
