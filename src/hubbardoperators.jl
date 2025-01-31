be7e1543c0c84f70b1e7557d27e2402e88e7d42d
Lukas Devos <ldevos98@gmail.com>
2025-01-31 09:29:21 -0500

    Unify module organization

--- src/hubbardoperators.jl
+++ src/hubbardoperators.jl
@@ -48,12 +48,173 @@ function hubbard_space(::Type{SU2Irrep}, ::Type{SU2Irrep})
     return Vect[FermionParity ⊠ SU2Irrep ⊠ SU2Irrep]((1, 1 // 2, 1 // 2) => 1)
 end
 
+# Single-site operators
+# ---------------------
 function single_site_operator(T, particle_symmetry::Type{<:Sector},
                               spin_symmetry::Type{<:Sector})
     V = hubbard_space(particle_symmetry, spin_symmetry)
     return zeros(T, V ← V)
 end
 
+@doc """
+    u_num([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    nꜛ([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+
+Return the one-body operator that counts the number of spin-up particles.
+""" u_num
+u_num(P::Type{<:Sector}, S::Type{<:Sector}) = u_num(ComplexF64, P, S)
+function u_num(T::Type{<:Number}, ::Type{Trivial}=Trivial, ::Type{Trivial}=Trivial)
+    t = single_site_operator(T, Trivial, Trivial)
+    I = sectortype(t)
+    t[(I(1), I(1))][1, 1] = 1
+    t[(I(0), I(0))][2, 2] = 1
+    return t
+end
+function u_num(T, ::Type{Trivial}, ::Type{U1Irrep})
+    t = single_site_operator(T, Trivial, U1Irrep)
+    I = sectortype(t)
+    t[(I(1, 1 // 2), dual(I(1, 1 // 2)))][1, 1] = 1
+    t[(I(0, 0), dual(I(0, 0)))][2, 2] = 1
+    return t
+end
+function u_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
+    throw(ArgumentError("`u_num` is not symmetric under `SU2Irrep` spin symmetry"))
+end
+function u_num(T, ::Type{U1Irrep}, ::Type{Trivial})
+    t = single_site_operator(T, U1Irrep, Trivial)
+    I = sectortype(t)
+    block(t, I(1, 1))[1, 1] = 1
+    block(t, I(0, 2))[1, 1] = 1
+    return t
+end
+function u_num(T, ::Type{U1Irrep}, ::Type{U1Irrep})
+    t = single_site_operator(T, U1Irrep, U1Irrep)
+    I = sectortype(t)
+    block(t, I(1, 1, 1 // 2)) .= 1
+    block(t, I(0, 2, 0)) .= 1
+    return t
+end
+function u_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
+    throw(ArgumentError("`u_num` is not symmetric under `SU2Irrep` spin symmetry"))
+end
+function u_num(T, ::Type{SU2Irrep}, ::Type{Trivial})
+    return error("Not implemented")
+end
+function u_num(T, ::Type{SU2Irrep}, ::Type{U1Irrep})
+    return error("Not implemented")
+end
+function u_num(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
+    throw(ArgumentError("`u_num` is not symmetric under `SU2Irrep` spin symmetry"))
+end
+const nꜛ = u_num
+
+@doc """
+    d_num([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    nꜜ([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+
+Return the one-body operator that counts the number of spin-down particles.
+""" d_num
+d_num(P::Type{<:Sector}, S::Type{<:Sector}) = d_num(ComplexF64, P, S)
+function d_num(T::Type{<:Number}, ::Type{Trivial}=Trivial, ::Type{Trivial}=Trivial)
+    t = single_site_operator(T, Trivial, Trivial)
+    I = sectortype(t)
+    t[(I(1), I(1))][2, 2] = 1
+    t[(I(0), I(0))][2, 2] = 1
+    return t
+end
+function d_num(T, ::Type{Trivial}, ::Type{U1Irrep})
+    t = single_site_operator(T, Trivial, U1Irrep)
+    I = sectortype(t)
+    t[(I(1, -1 // 2), dual(I(1, -1 // 2)))][1, 1] = 1
+    t[(I(0, 0), I(0, 0))][2, 2] = 1
+    return t
+end
+function d_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
+    throw(ArgumentError("`d_num` is not symmetric under `SU2Irrep` spin symmetry"))
+end
+function d_num(T, ::Type{U1Irrep}, ::Type{Trivial})
+    t = single_site_operator(T, U1Irrep, Trivial)
+    I = sectortype(t)
+    block(t, I(1, 1))[2, 2] = 1 # expected to be [1,2]
+    block(t, I(0, 2))[1, 1] = 1
+    return t
+end
+function d_num(T, ::Type{U1Irrep}, ::Type{U1Irrep})
+    t = single_site_operator(T, U1Irrep, U1Irrep)
+    I = sectortype(t)
+    block(t, I(1, 1, -1 // 2)) .= 1
+    block(t, I(0, 2, 0)) .= 1
+    return t
+end
+function d_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
+    throw(ArgumentError("`d_num` is not symmetric under `SU2Irrep` spin symmetry"))
+end
+function d_num(T, ::Type{SU2Irrep}, ::Type{Trivial})
+    return error("Not implemented")
+end
+function d_num(T, ::Type{SU2Irrep}, ::Type{U1Irrep})
+    return error("Not implemented")
+end
+function d_num(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
+    throw(ArgumentError("`d_num` is not symmetric under `SU2Irrep` spin symmetry"))
+end
+const nꜜ = d_num
+
+@doc """
+    c_num([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    n([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+
+Return the one-body operator that counts the number of particles.
+""" c_num
+c_num(P::Type{<:Sector}, S::Type{<:Sector}) = c_num(ComplexF64, P, S)
+function c_num(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
+    return u_num(T, particle_symmetry, spin_symmetry) +
+           d_num(T, particle_symmetry, spin_symmetry)
+end
+function c_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
+    t = single_site_operator(T, Trivial, SU2Irrep)
+    I = sectortype(t)
+    block(t, I(1, 1 // 2))[1, 1] = 1
+    block(t, I(0, 0))[2, 2] = 2
+    return t
+end
+function c_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
+    t = single_site_operator(T, U1Irrep, SU2Irrep)
+    I = sectortype(t)
+    block(t, I(1, 1, 1 // 2)) .= 1
+    block(t, I(0, 2, 0)) .= 2
+    return t
+end
+const n = c_num
+
+@doc """
+    ud_num([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+    nꜛꜜ([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
+
+Return the one-body operator that counts the number of doubly occupied sites.
+""" ud_num
+ud_num(P::Type{<:Sector}, S::Type{<:Sector}) = ud_num(ComplexF64, P, S)
+function ud_num(T, particle_symmetry::Type{<:Sector},
+                spin_symmetry::Type{<:Sector})
+    return u_num(T, particle_symmetry, spin_symmetry) *
+           d_num(T, particle_symmetry, spin_symmetry)
+end
+function ud_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
+    t = single_site_operator(T, Trivial, SU2Irrep)
+    I = sectortype(t)
+    block(t, I(0, 0))[2, 2] = 1
+    return t
+end
+function ud_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
+    t = single_site_operator(T, U1Irrep, SU2Irrep)
+    I = sectortype(t)
+    block(t, I(0, 2, 0)) .= 1
+    return t
+end
+const nꜛꜜ = ud_num
+
+# Two site operators
+# ------------------
 function two_site_operator(T, particle_symmetry::Type{<:Sector},
                            spin_symmetry::Type{<:Sector})
     V = hubbard_space(particle_symmetry, spin_symmetry)
@@ -268,162 +429,5 @@ function c_min_c_plus(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{
     return copy(adjoint(c_plus_c_min(T, particle_symmetry, spin_symmetry)))
 end
 const c⁻c⁺ = c_min_c_plus
-
-@doc """
-    u_num([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-    nꜛ([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-
-Return the one-body operator that counts the number of spin-up particles.
-""" u_num
-u_num(P::Type{<:Sector}, S::Type{<:Sector}) = u_num(ComplexF64, P, S)
-function u_num(T::Type{<:Number}, ::Type{Trivial}=Trivial, ::Type{Trivial}=Trivial)
-    t = single_site_operator(T, Trivial, Trivial)
-    I = sectortype(t)
-    t[(I(1), I(1))][1, 1] = 1
-    t[(I(0), I(0))][2, 2] = 1
-    return t
-end
-function u_num(T, ::Type{Trivial}, ::Type{U1Irrep})
-    t = single_site_operator(T, Trivial, U1Irrep)
-    I = sectortype(t)
-    t[(I(1, 1 // 2), dual(I(1, 1 // 2)))][1, 1] = 1
-    t[(I(0, 0), dual(I(0, 0)))][2, 2] = 1
-    return t
-end
-function u_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
-    throw(ArgumentError("`u_num` is not symmetric under `SU2Irrep` spin symmetry"))
-end
-function u_num(T, ::Type{U1Irrep}, ::Type{Trivial})
-    t = single_site_operator(T, U1Irrep, Trivial)
-    I = sectortype(t)
-    block(t, I(1, 1))[1, 1] = 1
-    block(t, I(0, 2))[1, 1] = 1
-    return t
-end
-function u_num(T, ::Type{U1Irrep}, ::Type{U1Irrep})
-    t = single_site_operator(T, U1Irrep, U1Irrep)
-    I = sectortype(t)
-    block(t, I(1, 1, 1 // 2)) .= 1
-    block(t, I(0, 2, 0)) .= 1
-    return t
-end
-function u_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
-    throw(ArgumentError("`u_num` is not symmetric under `SU2Irrep` spin symmetry"))
-end
-function u_num(T, ::Type{SU2Irrep}, ::Type{Trivial})
-    return error("Not implemented")
-end
-function u_num(T, ::Type{SU2Irrep}, ::Type{U1Irrep})
-    return error("Not implemented")
-end
-function u_num(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
-    throw(ArgumentError("`u_num` is not symmetric under `SU2Irrep` spin symmetry"))
-end
-const nꜛ = u_num
-
-@doc """
-    d_num([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-    nꜜ([particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-
-Return the one-body operator that counts the number of spin-down particles.
-""" d_num
-d_num(P::Type{<:Sector}, S::Type{<:Sector}) = d_num(ComplexF64, P, S)
-function d_num(T::Type{<:Number}, ::Type{Trivial}=Trivial, ::Type{Trivial}=Trivial)
-    t = single_site_operator(T, Trivial, Trivial)
-    I = sectortype(t)
-    t[(I(1), I(1))][2, 2] = 1
-    t[(I(0), I(0))][2, 2] = 1
-    return t
-end
-function d_num(T, ::Type{Trivial}, ::Type{U1Irrep})
-    t = single_site_operator(T, Trivial, U1Irrep)
-    I = sectortype(t)
-    t[(I(1, -1 // 2), dual(I(1, -1 // 2)))][1, 1] = 1
-    t[(I(0, 0), I(0, 0))][2, 2] = 1
-    return t
-end
-function d_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
-    throw(ArgumentError("`d_num` is not symmetric under `SU2Irrep` spin symmetry"))
-end
-function d_num(T, ::Type{U1Irrep}, ::Type{Trivial})
-    t = single_site_operator(T, U1Irrep, Trivial)
-    I = sectortype(t)
-    block(t, I(1, 1))[2, 2] = 1 # expected to be [1,2]
-    block(t, I(0, 2))[1, 1] = 1
-    return t
-end
-function d_num(T, ::Type{U1Irrep}, ::Type{U1Irrep})
-    t = single_site_operator(T, U1Irrep, U1Irrep)
-    I = sectortype(t)
-    block(t, I(1, 1, -1 // 2)) .= 1
-    block(t, I(0, 2, 0)) .= 1
-    return t
-end
-function d_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
-    throw(ArgumentError("`d_num` is not symmetric under `SU2Irrep` spin symmetry"))
-end
-function d_num(T, ::Type{SU2Irrep}, ::Type{Trivial})
-    return error("Not implemented")
-end
-function d_num(T, ::Type{SU2Irrep}, ::Type{U1Irrep})
-    return error("Not implemented")
-end
-function d_num(T, ::Type{SU2Irrep}, ::Type{SU2Irrep})
-    throw(ArgumentError("`d_num` is not symmetric under `SU2Irrep` spin symmetry"))
-end
-const nꜜ = d_num
-
-@doc """
-    c_num([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-    n([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-
-Return the one-body operator that counts the number of particles.
-""" c_num
-c_num(P::Type{<:Sector}, S::Type{<:Sector}) = c_num(ComplexF64, P, S)
-function c_num(T, particle_symmetry::Type{<:Sector}, spin_symmetry::Type{<:Sector})
-    return u_num(T, particle_symmetry, spin_symmetry) +
-           d_num(T, particle_symmetry, spin_symmetry)
-end
-function c_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
-    t = single_site_operator(T, Trivial, SU2Irrep)
-    I = sectortype(t)
-    block(t, I(1, 1 // 2))[1, 1] = 1
-    block(t, I(0, 0))[2, 2] = 2
-    return t
-end
-function c_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
-    t = single_site_operator(T, U1Irrep, SU2Irrep)
-    I = sectortype(t)
-    block(t, I(1, 1, 1 // 2)) .= 1
-    block(t, I(0, 2, 0)) .= 2
-    return t
-end
-const n = c_num
-
-@doc """
-    ud_num([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-    nꜛꜜ([T], [particle_symmetry::Type{<:Sector}], [spin_symmetry::Type{<:Sector}])
-
-Return the one-body operator that counts the number of doubly occupied sites.
-""" ud_num
-ud_num(P::Type{<:Sector}, S::Type{<:Sector}) = ud_num(ComplexF64, P, S)
-function ud_num(T, particle_symmetry::Type{<:Sector},
-                spin_symmetry::Type{<:Sector})
-    return u_num(T, particle_symmetry, spin_symmetry) *
-           d_num(T, particle_symmetry, spin_symmetry)
-end
-function ud_num(T, ::Type{Trivial}, ::Type{SU2Irrep})
-    t = single_site_operator(T, Trivial, SU2Irrep)
-    I = sectortype(t)
-    block(t, I(0, 0))[2, 2] = 1
-    return t
-end
-function ud_num(T, ::Type{U1Irrep}, ::Type{SU2Irrep})
-    t = single_site_operator(T, U1Irrep, SU2Irrep)
-    I = sectortype(t)
-    block(t, I(0, 2, 0)) .= 1
-    return t
-end
-const nꜛꜜ = ud_num
 
 end
