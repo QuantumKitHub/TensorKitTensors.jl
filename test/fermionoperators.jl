4cfc3451ceb3b86fe88f3c22602822e6d3fc74f8
sanderdemeyer <sander.demeyer@ugent.be>
2025-03-28 14:16:40 +0100

    changed definition of SF
    Only allow for contracted tensors. Also changed some naming conventions to match the ones in this package

--- test/fermionoperators.jl
+++ test/fermionoperators.jl
@@ -11,24 +11,20 @@ using StableRNGs
 # {cᵢ, cⱼ†} = δᵢⱼ
 
 @testset "simple fermions" begin
-    cc = contract_twosite(c⁻(; side=:L), c⁻(; side=:R))
-    cc⁺ = contract_twosite(c⁻(; side=:L), c⁺(; side=:R))
-    c⁺c = contract_twosite(c⁺(; side=:L), c⁻(; side=:R))
-    c⁺c⁺ = contract_twosite(c⁺(; side=:L), c⁺(; side=:R))
-
-    @test cc ≈ -permute(cc, ((2, 1), (4, 3)))
-    @test c⁺c⁺ ≈ -permute(c⁺c⁺, ((2, 1), (4, 3)))
+    @test c⁻c⁻() ≈ -permute(c⁻c⁻(), ((2, 1), (4, 3)))
+    @test c⁺c⁺() ≈ -permute(c⁺c⁺(), ((2, 1), (4, 3)))
 
     # the following doesn't hold
     # I don't think I can get all of these to hold simultaneously?
     # @test cc⁺ ≈ -permute(c⁺c, (2, 1), (4, 3))
 
-    @test cc⁺' ≈ c⁺c
-    @test cc' ≈ c⁺c⁺
-    @test (c⁺c + cc⁺)' ≈ cc⁺ + c⁺c
-    @test (c⁺c - cc⁺)' ≈ cc⁺ - c⁺c
+    @test c⁻c⁺()' ≈ c⁺c⁻()
+    @test c⁻c⁻()' ≈ c⁺c⁺()
+    @test (c⁺c⁻() + c⁻c⁺())' ≈ c⁻c⁺() + c⁺c⁻()
+    @test (c⁺c⁻() - c⁻c⁺())' ≈ c⁻c⁺() - c⁺c⁻()
 
-    @test c_number() ≈ contract_onesite(c⁺(; side=:L), c⁻(; side=:R))
+    @plansor c_number[-1; -2] := c⁺c⁻()[-1 1; 3 2] * τ[3 2; -2 1]
+    @test c_number ≈ c_num()
 end
 
 @testset "Exact Diagonalization" begin
@@ -38,12 +34,9 @@ end
     t, V, mu = rand(rng, 3)
     pspace = Vect[fℤ₂](0 => 1, 1 => 1)
 
-    cc⁺ = contract_twosite(c⁻(; side=:L), c⁺(; side=:R))
-    c⁺c = contract_twosite(c⁺(; side=:L), c⁻(; side=:R))
-
-    H = -t * (cc⁺ + c⁺c) +
-        V * ((c_number() - 0.5 * id(pspace)) ⊗ (c_number() - 0.5 * id(pspace))) -
-        0.5 * mu * (c_number() ⊗ id(pspace) + id(pspace) ⊗ c_number())
+    H = -t * (c⁻c⁺() + c⁺c⁻()) +
+        V * ((n() - 0.5 * id(pspace)) ⊗ (n() - 0.5 * id(pspace))) -
+        0.5 * mu * (n() ⊗ id(pspace) + id(pspace) ⊗ n())
     # Values based on https://arxiv.org/abs/1610.05003v1. Half-Chain Entanglement Entropy in the One-Dimensional Spinless Fermion Model
     true_eigenvalues = sort([V / 4, V / 4 - mu, -V / 4 - mu / 2 + t, -V / 4 - mu / 2 - t])
 
