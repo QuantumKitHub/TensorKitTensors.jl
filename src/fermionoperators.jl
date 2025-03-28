c2509badf12e77fd2840b014c2119f96dd196690
sanderdemeyer <sander.demeyer@ugent.be>
2025-03-28 14:25:31 +0100

    fix docs

--- src/fermionoperators.jl
+++ src/fermionoperators.jl
@@ -13,11 +13,12 @@ export c⁺, c⁻
 export c⁺c⁻, c⁻c⁺, c⁺c⁺, c⁻c⁻
 export n
 
-"""
+@doc """
     c_num([elt::Type{<:Number}=ComplexF64])
+    n([elt::Type{<:Number}=ComplexF64])
 
-Fermionic number operator.
-"""
+Return the one-body operator that counts the nunber of particles.
+""" c_num
 function c_num(T::Type{<:Number}=ComplexF64)
     pspace = Vect[fℤ₂](0 => 1, 1 => 1)
     n = zeros(T, pspace ← pspace)
@@ -33,7 +34,13 @@ function two_site_operator(T::Type{<:Number}=ComplexF64)
     return zeros(T, V ⊗ V ← V ⊗ V)
 end
 
-function c_plus_c_min(T=ComplexF64)
+@doc """
+    c_plus_c_min([elt::Type{<:Number}=ComplexF64])
+    c⁺c⁻([elt::Type{<:Number}=ComplexF64])
+
+Return the two-body operator that creates a particle at the first site and annihilates a particle at the second.
+""" c_plus_c_min
+function c_plus_c_min(T::Type{<:Number}=ComplexF64)
     t = two_site_operator(T)
     I = sectortype(t)
     t[(I(1), I(0), dual(I(0)), dual(I(1)))] .= 1
@@ -41,7 +48,13 @@ function c_plus_c_min(T=ComplexF64)
 end
 const c⁺c⁻ = c_plus_c_min
 
-function c_min_c_plus(T=ComplexF64)
+@doc """
+    c_min_c_plus([elt::Type{<:Number}=ComplexF64])
+    c⁻c⁺([elt::Type{<:Number}=ComplexF64])
+
+Return the two-body operator that annihilates a particle at the first site and creates a particle at the second.
+""" c_min_c_plus
+function c_min_c_plus(T::Type{<:Number}=ComplexF64)
     t = two_site_operator(T)
     I = sectortype(t)
     t[(I(0), I(1), dual(I(1)), dual(I(0)))] .= 1
@@ -49,7 +62,13 @@ function c_min_c_plus(T=ComplexF64)
 end
 const c⁻c⁺ = c_min_c_plus
 
-function c_plus_c_plus(T=ComplexF64)
+@doc """
+    c_plus_c_plus([elt::Type{<:Number}=ComplexF64])
+    c⁺c⁺([elt::Type{<:Number}=ComplexF64])
+
+Return the two-body operator that creates a particle at the first and at the second site.
+""" c_plus_c_plus
+function c_plus_c_plus(T::Type{<:Number}=ComplexF64)
     t = two_site_operator(T)
     I = sectortype(t)
     t[(I(1), I(1), dual(I(0)), dual(I(0)))] .= 1
@@ -57,7 +76,13 @@ function c_plus_c_plus(T=ComplexF64)
 end
 const c⁺c⁺ = c_plus_c_plus
 
-function c_min_c_min(T=ComplexF64)
+@doc """
+    c_min_c_min([elt::Type{<:Number}=ComplexF64])
+    c⁻c⁻([elt::Type{<:Number}=ComplexF64])
+
+Return the two-body operator that annihilates a particle at the first and at the second site.
+""" c_min_c_min
+function c_min_c_min(T::Type{<:Number}=ComplexF64)
     t = two_site_operator(T)
     I = sectortype(t)
     t[(I(0), I(0), dual(I(1)), dual(I(1)))] .= 1
