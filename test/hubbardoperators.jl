7f42aad13e045d2a407ff409784789b40db2bcd3
Sander De Meyer <74001142+sanderdemeyer@users.noreply.github.com>
2025-06-11 14:01:28 +0200

    Update test/hubbardoperators.jl

--- test/hubbardoperators.jl
+++ test/hubbardoperators.jl
@@ -93,11 +93,17 @@ end
             if particle_symmetry == Trivial && spin_symmetry == Trivial
                 uu = u_min_u_min(particle_symmetry, spin_symmetry)
                 dd = d_min_d_min(particle_symmetry, spin_symmetry)
+                upup = u_plus_u_plus(particle_symmetry, spin_symmetry)
+                dpdp = d_plus_d_plus(particle_symmetry, spin_symmetry)
                 @test swap_2sites(uu) ≈ -uu
                 @test swap_2sites(dd) ≈ -dd
+                @test swap_2sites(upup) ≈ -upup
+                @test swap_2sites(dpdp) ≈ -dpdp
             else
                 @test_throws ArgumentError u_min_u_min(particle_symmetry, spin_symmetry)
                 @test_throws ArgumentError d_min_d_min(particle_symmetry, spin_symmetry)
+                @test_throws ArgumentError u_plus_u_plus(particle_symmetry, spin_symmetry)
+                @test_throws ArgumentError d_plus_d_plus(particle_symmetry, spin_symmetry)
             end
 
             # test spin operator
